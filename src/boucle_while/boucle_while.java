c3befe3ff952f1f545597b0611d243511d908dc7
CyrilLeducq <cyril.leducq.pro@gmail.com>
2025-04-30 10:55:44 +0200

    Do_while

---
+++ src/boucle_while/boucle_while.java
@@ -0,0 +1,20 @@
+package boucle_while;
+
+public class boucle_while {
+    public static void main(String[] args){
+
+        int x = 1;
+        //Boucle while
+        while (x < 10){
+            System.out.println(x);
+            x++;
+        }
+
+        //Boucle do-while
+        x = 1;
+        do {
+            System.out.println(x);
+            x++;
+        } while (x < 10 );
+    }
+}
