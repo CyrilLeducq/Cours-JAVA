79950dbab73206b27d425ebefbb32a8a3cef7a29
Cyril <cyril.leducq.pro@gmail.com>
2025-05-06 14:55:32 +0200

    Le polymorphisme

---
+++ override_polymorphisme/src/com/linkedin/Main.java
@@ -0,0 +1,19 @@
+package com.linkedin;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Chat chat = new Chat ("Caramel", 25, 10, 2, "noir");
+        chat.manger();
+        chat.seDeplacer();
+
+        Animal animal = new Chat("Minou", 15 , 8, 1, "Blanc");
+        animal.manger();
+
+        animal = new Chien("Rex", 15, 8, 1);
+        animal.manger();
+
+        animal = new Animal("Ani", 20, 12, 2);
+    }
+}
